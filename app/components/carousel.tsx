507738c98592b56b6c2cbe81c050d9651d6c2f76
Giacomo <giacomo.bressan@yahoo.it>
2025-01-18 14:33:40 +0100

    varie

--- app/components/carousel.tsx
+++ app/components/carousel.tsx
@@ -1,23 +1,10 @@
 "use client";
-import { useState } from "react";
 
 interface CarouselProps {
   images: string[]; // Array of image URLs
 }
 
 const Carousel: React.FC<CarouselProps> = ({ images }) => {
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
-
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-  };
-
-  const handlePrev = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
-    );
-  };
-
   return (
     <div className="w-full md:h-[90vh] h-[70vh] bg-customGreen flex items-center justify-center text-customBlue relative">
       <div className="bg-customGray xl:w-[1000px]  lg:h-[300px] lg:w-[900px] md:w-[680px] md:h-[250px] w-[350px] h-[185px] text-3xl p-6 pl-[10%] pr-[20%] relative md:mt-0 mt-14">
@@ -41,7 +28,7 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
       </div>
       <div className="bg-customGray xl:h-[500px] xl:w-[500px] lg:h-[420px] lg:w-[420px] md:h-[320px] md:w-[320px] h-[205px] w-[205px] absolute rounded-full md:translate-x-3/4 md:translate-y-1/4 md:top-0 top-12 flex justify-center items-center">
         <img
-          src={images[currentIndex]}
+          src={images[0]}
           alt="Carousel Image"
           className="xl:h-[70%] xl:w-[70%] h-[70%] w-[70%] object-cover"
         />
